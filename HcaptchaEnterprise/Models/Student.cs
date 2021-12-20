aa9656880ce216822b8651c58f48173f6538af6a
AnSharma <AnSharma@LAP-55379>
2021-12-20 16:59:19 +0530

    Add project files.

---
+++ HcaptchaEnterprise/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace HcaptchaEnterprise.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string token { get; set; }
+    }
+}
